e12b22eab1793b118798cd58076e22cfe633c3da
subhram260 <subhram336@gmail.com>
2025-10-25 16:40:48 +0530

    dd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@ export const environment = {
   production: false,
   // apiUrl: 'https://policyportal-api.onrender.com/api'
   // apiUrl: 'https://policyportal-api-deploytesting.onrender.com/api'
-  apiUrl: 'https://policyportal-nodejs.onrender.com/api'
+  apiUrl: 'https://policyportal-nodejs.onrender.com/api' 
+  
   // apiUrl: 'http://localhost:5001/api'
 };
 
